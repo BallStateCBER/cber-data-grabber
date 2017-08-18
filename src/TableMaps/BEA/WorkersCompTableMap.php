8879fd370d4332aced874ce3177b3e3b58200da3
Graham Watson <graham@phantomwatson.com>
2017-08-18 15:51:45 -0400

    Clean up property doc comments

--- src/TableMaps/BEA/WorkersCompTableMap.php
+++ src/TableMaps/BEA/WorkersCompTableMap.php
@@ -6,13 +6,14 @@ class WorkersCompTableMap extends TableMap
     protected static $DATASET = 'RegionalIncome';
     protected static $TABLE = 'CA35';
 
-    // BEA Line Code => Plain English Column Name
+    /** @var array $MAP BEA Line Code => Plain English Column Name */
     protected static $MAP = [
         '2120' => 'Retirement and Disability Insurance Benefits, Excluding Social Security'
     ];
 
-    /*
-     * Group Name => comma delimited list of column identifiers for the group
+    /**
+     * @var array $GROUPS Group Name => comma delimited list of column identifiers for the group
+     *
      * (an identifier is a string that matches values in $MAP that are part of the group,
      * and does not match any $MAP values outside of the group)
      */
